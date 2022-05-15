ba4e495b1f01f9d49c4f276f23f26896b2b7a8d6
HamiltonLopes <hamilton_dino02@hotmail.com>
2022-05-15 20:54:40 -0300

    qatt

--- src/projetinho.ts
+++ src/projetinho.ts
@@ -1,5 +1,42 @@
-function soma(a: number, b:number){
-    return a+ b;
-}
+// let nome: string;
 
-soma(1,2);
+// nome = 'Hamilton';
+
+// function nomeCompleto(nome: string, sobrenome :string){
+//     return `${nome} ${sobrenome}`;
+// }
+
+// console.log(nomeCompleto(nome,'Lopes'));
+
+// //lol
+// let x: string;
+// let y: null;
+
+// function sum(one:number, two?:number) { 
+//     const result = one + two;
+
+//     if(two==0)
+    
+//     result = one+one;
+    
+    
+    
+//     return result ;
+    
+// }
+    
+    
+    
+//     let result = sum(2); 
+//     console.log(result)
+
+    function print(){
+
+        enum Fruta { MACA = 1, BANANA, PERA,}
+        
+        let fruta: string = Fruta[2]; 
+        console.log(fruta);
+        
+    }
+        
+        print()
